517f8e0df059397b335d1323c89d2555ee11fe34
dm-develop <dm_dev@mailbox.org>
2023-04-29 14:57:03 +0200

    changes wip

---
+++ ModSetup.cs
@@ -0,0 +1,142 @@
+﻿using Il2Cpp;
+using Il2CppInterop.Runtime;
+using MelonLoader;
+using UnityEngine;
+using Type = Il2CppSystem.Type;
+
+namespace dm.ffmods.raidersdroploot
+{
+    public enum LootItem
+    {
+        crudeWeapon,
+        weapon,
+        heavyWeapon,
+        leatherCoat,
+        hauberk,
+        plateMail,
+        shield,
+        bow,
+        crossbow,
+        arrows,
+        smokedMeat,
+        bread
+    }
+
+    public class ModSetup
+    {
+        #region Fields
+
+        public static Dictionary<LootItem, string> ItemNames = new Dictionary<LootItem, string>()
+        {
+            { LootItem.crudeWeapon, "Melee_Club01A_Resource" },
+            { LootItem.hauberk, "Resource_Villager_Hauberk01A" },
+            { LootItem.heavyWeapon , "Melee_Mace01A_Resource" },
+            { LootItem.leatherCoat , "Resource_Villager_HideCoat01A"},
+            { LootItem.plateMail , "Resource_Villager_Platemail01A"},
+            { LootItem.weapon , "Melee_Sword01A_Resource"},
+            { LootItem.shield, "Resource_Villager_Shield01A"},
+            { LootItem.bow , "Ranged_Bow01A_Resource"},
+            { LootItem.crossbow , "Ranged_Crossbow01A_Resource"},
+            { LootItem.arrows, "Ranged_Bow01A_Ammo_Resource"},
+            { LootItem.smokedMeat , "Resource_Villager_SmokedMeat01A"},
+            { LootItem.bread , "Resource_Villager_Bread01A"}
+        };
+
+        public bool ArePrefabsMissing = true;
+
+        public Dictionary<LootItem, GameObject> ItemPrefabs = new Dictionary<LootItem, GameObject>();
+
+        public Dictionary<LootItem, SpawnConfig> SpawnCOnfigs = new Dictionary<LootItem, SpawnConfig>();
+
+        #endregion Fields
+
+        #region Public Methods
+
+        public void CreateSpawnConfigs(GameManager gameManager, LootItem item)
+        {
+            switch (item)
+            {
+                case LootItem.crudeWeapon:
+                    SpawnCOnfigs.Add(new SpawnConfig(ItemPrefabs[item].GetComponentInChildren<WeaponResource>(),);
+                    break;
+
+                case y:
+                    // code block
+                    break;
+
+                default:
+                    // code block
+                    break;
+            }
+
+            WeaponResource prefab = AssignPrefab();
+
+            return new SpawnConfig(prefab, gameManager.workBucketManager.itemWeapon, action);
+
+            // the action to package into the config
+            void action(DroppedResource instance)
+            {
+                gameManager.resourceManager.AddOrRemoveWeaponResource(instance.Cast<WeaponResource>(), false);
+            }
+        }
+
+        public bool TryFindItemPrefabs()
+        {
+            // get all objects (also inactive and in "hidden scenes")
+            Type val = Il2CppType.Of<GameObject>();
+            var allObjects = Resources.FindObjectsOfTypeAll(val); // this is expensive!
+            List<LootItem> itemsMissing = ItemNames.Keys.Except(ItemPrefabs.Keys).ToList();
+
+            // check for all objects
+            foreach (var obj in allObjects)
+            {
+                // is current object one of the prefabs we are still looking for
+                foreach (var item in ItemNames)
+                {
+                    if (!itemsMissing.Contains(item.Key))
+                    {
+                        continue;
+                    }
+                    if (obj.Cast<GameObject>().name == item.Value)
+                    {
+                        ItemPrefabs.Add(item.Key, obj.Cast<GameObject>());
+                        itemsMissing.Remove(item.Key);
+                        Melon<RaidersDropLoot>.Logger.Msg($"found prefab for '{item.Key}!'");
+                    }
+                }
+            }
+            // report whether we found all prefabs
+            if (itemsMissing.Count > 0)
+            {
+                ArePrefabsMissing = true;
+            }
+            if (itemsMissing.Count == 0)
+            {
+                ArePrefabsMissing = false;
+            }
+            return !ArePrefabsMissing;
+        }
+
+        #endregion Public Methods
+
+        #region Private Methods
+
+        private WeaponResource AssignPrefab()
+        {
+            Type val = Il2CppType.Of<GameObject>();
+            var allObjects = Resources.FindObjectsOfTypeAll(val);
+
+            foreach (var obj in allObjects)
+            {
+                if (obj.Cast<GameObject>().name == "Melee_Sword01A_Resource")
+                {
+                    return obj.Cast<GameObject>().GetComponentInChildren<WeaponResource>();
+                }
+            }
+            // should never be reached, because we check before
+            return new WeaponResource();
+        }
+
+        #endregion Private Methods
+    }
+}
---
+++ RaidersDropLoot.cs
@@ -0,0 +1,89 @@
+﻿using Il2Cpp;
+using MelonLoader;
+using UnityEngine;
+
+namespace dm.ffmods.raidersdroploot
+{
+    public class RaidersDropLoot : MelonMod
+    {
+        #region Fields
+
+        public GameManager GameManager;
+        private float checkIntervalInSeconds = 1f;
+        private bool frontierHasLoaded = false;
+        private ModSetup modSetup;
+        private float timeSinceLastCheckInSeconds = 0f;
+
+        #endregion Fields
+
+        #region Properties
+
+        public bool HasInitalised { get; private set; }
+
+        #endregion Properties
+
+        #region Public Methods
+
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Raiders drop loot mod loaded!");
+            modSetup = new ModSetup();
+        }
+
+        public override void OnLateUpdate()
+        {
+            if (HasInitalised)
+            {
+                return;
+            }
+            // only continue if timer says so
+            timeSinceLastCheckInSeconds += Time.deltaTime;
+            if (timeSinceLastCheckInSeconds < checkIntervalInSeconds)
+            {
+                return;
+            }
+            // only continue if main scene has loaded
+            if (!frontierHasLoaded)
+            {
+                return;
+            }
+            // only continue if GaneManager can been found
+            if ((System.Object)(object)GameObject.Find("GameManager") == (System.Object)null)
+            {
+                LoggerInstance.Msg($"could not find gameManager instance, will try again in {checkIntervalInSeconds} ...");
+                timeSinceLastCheckInSeconds = 0;
+                return;
+            }
+            // find GaneManager
+            GameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+            // only continue if sword can be found
+            if (!modSetup.TryFindItemPrefabs())
+            {
+                LoggerInstance.Msg($"could not find all item prefabs, will try again in {checkIntervalInSeconds} ...");
+                timeSinceLastCheckInSeconds = 0;
+                return;
+            }
+
+            if (!modSetup.ArePrefabsMissing)
+            {
+                LoggerInstance.Msg($"all prefabs found, creating SpawnConfigs ...");
+                Config = modSetup.CreateConfigs(GameManager);
+            }
+
+            HasInitalised = true;
+            LoggerInstance.Msg($"mod initialised!");
+        }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            LoggerInstance.Msg($"Scene {sceneName} with build index {buildIndex} has been loaded!");
+            if (sceneName == "Frontier")
+            {
+                frontierHasLoaded = true;
+            }
+        }
+
+        #endregion Public Methods
+    }
+}
